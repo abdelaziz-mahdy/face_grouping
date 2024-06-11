685932386d73a6d24bd99cf42c2307d95c73ca9d
Abdelaziz Mahdy <zezohassam@gmail.com>
2024-06-11 17:59:37 +0300

    initial setup to switch to detector

--- lib/image_service.dart
+++ lib/image_service.dart
@@ -1,6 +1,8 @@
 import 'dart:async';
 import 'dart:io';
 import 'dart:isolate';
+import 'dart:typed_data';
+
 import 'package:flutter/services.dart';
 import 'package:opencv_dart/opencv_dart.dart' as cv;
 import 'package:path_provider/path_provider.dart';
@@ -57,21 +59,7 @@ class ImageService {
   factory ImageService() {
     return instance;
   }
-
-  bool _xmlLoaded = false;
-  String? _haarcascadesPath;
-
-  Future<void> _loadXml() async {
-    if (!_xmlLoaded) {
-      final tmpDir = await getTemporaryDirectory();
-      _haarcascadesPath = '${tmpDir.path}/haarcascade_frontalface_default.xml';
-
-      await _copyAssetFileToTmp(
-          'assets/haarcascade_frontalface_default.xml', _haarcascadesPath!);
-      _xmlLoaded = true;
-    }
-  }
-
+  
   Future<void> _copyAssetFileToTmp(String assetPath, String tmpPath) async {
     final byteData = await rootBundle.load(assetPath);
     final file = File(tmpPath);
@@ -80,15 +68,12 @@ class ImageService {
 
   Future<List<ImageData>> processDirectory(String directoryPath,
       void Function(double, Duration, int, int) progressCallback) async {
-    await _loadXml();
     final completer = Completer<List<ImageData>>();
     final receivePort = ReceivePort();
     final startTime = DateTime.now();
 
-    Isolate.spawn(
-        _processDirectoryIsolate,
-        _ProcessDirectoryParams(
-            directoryPath, receivePort.sendPort, _haarcascadesPath!));
+    Isolate.spawn(_processDirectoryIsolate,
+        _ProcessDirectoryParams(directoryPath, receivePort.sendPort));
 
     receivePort.listen((message) {
       if (message is _ProgressMessage) {
@@ -115,12 +100,17 @@ class ImageService {
         .where((entity) => entity is File && _isImageFile(entity.path))
         .toList();
 
+    const modelPath = "assets/face_detection_yunet_2023mar.onnx";
+    final buf = (await rootBundle.load(modelPath)).buffer.asUint8List();
+    final faceDetector =
+        cv.FaceDetectorYN.fromBuffer("onnx", buf, Uint8List(0), (320, 320));
+
+    // Initialize FaceDetectorYN with required parameters.
     final totalImages = imageFiles.length;
 
     for (var i = 0; i < imageFiles.length; i++) {
       final entity = imageFiles[i] as File;
-      final faceRects =
-          await _detectFaces(entity.path, params.haarcascadesPath);
+      final faceRects = await _detectFaces(entity.path, faceDetector);
       final sendableRects =
           faceRects.map((rect) => SendableRect.fromRect(rect)).toList();
       final faceImages = await _extractFaces(entity.path, sendableRects);
@@ -144,13 +134,19 @@ class ImageService {
         .any((ext) => path.toLowerCase().endsWith(ext));
   }
 
-  static Future<cv.VecRect> _detectFaces(
-      String imagePath, String haarcascadesPath) async {
+  static Future<List<cv.Rect>> _detectFaces(
+      String imagePath, cv.FaceDetectorYN detector) async {
     final img = cv.imread(imagePath, flags: cv.IMREAD_COLOR);
-    final classifier = cv.CascadeClassifier.empty();
-    classifier.load(haarcascadesPath);
-    final rects = classifier.detectMultiScale(img);
-    return rects;
+    detector.setInputSize((img.width, img.height));
+    final faces = detector.detect(img);
+    return List.generate(faces.rows, (i) {
+      return cv.Rect(
+        faces.at<double>(i, 0).toInt(),
+        faces.at<double>(i, 1).toInt(),
+        faces.at<double>(i, 2).toInt(),
+        faces.at<double>(i, 3).toInt(),
+      );
+    });
   }
 
   static Future<List<Uint8List>> _extractFaces(
@@ -171,10 +167,8 @@ class ImageService {
 class _ProcessDirectoryParams {
   final String directoryPath;
   final SendPort sendPort;
-  final String haarcascadesPath;
 
-  _ProcessDirectoryParams(
-      this.directoryPath, this.sendPort, this.haarcascadesPath);
+  _ProcessDirectoryParams(this.directoryPath, this.sendPort);
 }
 
 class _ProgressMessage {
